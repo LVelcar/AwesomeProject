b70df4bbb315c21fa694a1f9fd047bf3e7b93a46
LVelcar <luisvelcar9@gmail.com>
2023-09-20 07:59:58 -0700

    update validation

--- App.js
+++ App.js
@@ -3,19 +3,44 @@ import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet } from 'reac
 
 export default function App() {
 
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const isEmailValid = (email) => {
+    // Expresión regular para validar correos electrónicos
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    return emailRegex.test(email);
+  };
+
+  const isPasswordValid = (password) => {
+    // Verifica que la contraseña tenga al menos 8 caracteres y contenga al menos una mayúscula y un dígito
+    return /^(?=.*[A-Z])(?=.*\d).{8,}$/.test(password);
+  };
+
+  const handleSubmit = () => {
+    if (!isEmailValid(email)) {
+      alert('Ingrese un correo electrónico válido');
+    } else if (!isPasswordValid(password)) {
+      alert('La contraseña debe tener al menos 8 caracteres, incluyendo una mayúscula y un número.');
+    } else {
+      // Lógica de inicio de sesión
+      console.log(email);
+    }
+  };
+  
 
   return (
     <View style={styles.container}>
-
+      <Image style={styles.logo}
+        source={require("./assets/fantasma.png")}>
+      </Image>
       <Text style={styles.title}>ToDo App</Text>
       <TextInput
         style={styles.input}
-        placeholder="Nombre de Usuario"
+        placeholder="Email"
         placeholderTextColor="#888"
-        value={username}
-        onChangeText={text => setUsername(text)}
+        value={email}
+        onChangeText={text => setEmail(text)}
       />
       <TextInput
         style={styles.input}
@@ -25,13 +50,7 @@ export default function App() {
         value={password}
         onChangeText={text => setPassword(text)}
       />
-      <TouchableOpacity style={styles.loginButton} onPress={() => {
-          if (username.trim() === '' || password.trim() === '') {
-            alert('Por favor, complete todos los campos');
-          } else {
-            // Aquí puedes realizar la lógica de inicio de sesión
-          }
-        }}>
+      <TouchableOpacity style={styles.loginButton} onPress={handleSubmit}>
         <Text style={styles.loginButtonText}>Iniciar Sesión</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.forgotPassword}>
