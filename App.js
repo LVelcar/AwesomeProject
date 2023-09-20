37be49319a6cd70139e146e8b19d9d3f22bef581
LVelcar <luisvelcar9@gmail.com>
2023-09-19 22:55:00 -0700

    Update App

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet } from 'react-native';
 
 export default function App() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style={styles.title}>ToDo App</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nombre de Usuario"
+        placeholderTextColor="#888"
+        value={username}
+        onChangeText={text => setUsername(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        placeholderTextColor="#888"
+        secureTextEntry
+        value={password}
+        onChangeText={text => setPassword(text)}
+      />
+      <TouchableOpacity style={styles.loginButton} onPress={() => {
+          if (username.trim() === '' || password.trim() === '') {
+            alert('Por favor, complete todos los campos');
+          } else {
+            // Aquí puedes realizar la lógica de inicio de sesión
+          }
+        }}>
+        <Text style={styles.loginButtonText}>Iniciar Sesión</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.forgotPassword}>
+        <Text style={styles.forgotPasswordText}>¿Olvidó su contraseña?</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,5 +47,43 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 100, 
+    height: 100, 
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  loginButton: {
+    width: '80%',
+    height: 40,
+    backgroundColor: '#007AFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  loginButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  forgotPassword: {
+    marginTop: 10,
+  },
+  forgotPasswordText: {
+    color: '#007AFF',
+    fontSize: 16,
   },
 });
